6a5dfbbdda8422f256a328a98747cd3a44054cd2
zengweigang <zengweigang@yongdaxing.cn>
2021-06-17 16:20:24 +0800

    fix paswd

--- src/pages/supplier-management/supplier-list/consultant-list/password.jsx
+++ src/pages/supplier-management/supplier-list/consultant-list/password.jsx
@@ -25,7 +25,7 @@ export default (props) => {
     }
   };
 
-  const submit = () => {
+  const submit = (password) => {
     editPwd({
       password: md5(password),
       accountId: data.bindAccountId
