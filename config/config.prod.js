d94c65fea2c5bf7dc299469ff7cdf35cfb26ee6c
huyueke <huyueke@yongdaxing.cn>
2021-07-24 12:26:46 +0800

    http+s

--- config/config.prod.js
+++ config/config.prod.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   define: {
     API_URL: 'https://adminapi.yeahgo.com',
-    MARKET_URL: 'http://publicmobile.yeahgo.com',
+    MARKET_URL: 'https://publicmobile.yeahgo.com',
   },
 });
