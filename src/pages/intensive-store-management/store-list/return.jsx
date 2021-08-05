57caaa11d9fa107206d5ecb0877c382cc6447131
zengweigang <zengweigang@yongdaxing.cn>
2021-08-05 17:25:48 +0800

    text

--- src/pages/intensive-store-management/store-list/return.jsx
+++ src/pages/intensive-store-management/store-list/return.jsx
@@ -9,7 +9,7 @@ import { getDetail } from '@/services/intensive-store-management/store-detail';
 import { amountTransform } from '@/utils/utils'
 
 export default (props) => {
-  const { visible, setVisible, callback, data, type } = props;
+  const { visible, setVisible, callback, data } = props;
   const [deposit, setDeposit] = useState({})
   const [form] = Form.useForm();
   const formItemLayout = {
@@ -29,7 +29,7 @@ export default (props) => {
     return new Promise((resolve, reject) => {
       let userInfo = window.localStorage.getItem('user');
       userInfo = userInfo && JSON.parse(userInfo);
-      const apiMethod = type === 1 ? refunded : backTrack
+      const apiMethod = data.type === 1 ? refunded : backTrack
       apiMethod({
         applyId: data.applyId,
         refendAmount: amountTransform(values.refendAmount),
@@ -58,7 +58,7 @@ export default (props) => {
 
   return (
     <ModalForm
-      title={`请登记店铺保证金退回信息  ${data.storeName} （店铺ID：${data.id}）`}
+      title={`请${data?.type === 1 ? '登记店铺保证金退回信息' : '确认将保证金原路退回信息'}  ${data.storeName} （店铺ID：${data.id}）`}
       modalProps={{
       }}
       onVisibleChange={setVisible}
@@ -70,27 +70,33 @@ export default (props) => {
         callback();
         return true;
       }}
+      submitter={{
+        searchConfig: {
+          submitText: data?.type === 1 ? '确定' : '立即原来退回保证金',
+          resetText: '取消'
+        }
+      }}
       {...formItemLayout}
     >
       <Form.Item
         label="保证金缴纳状态"
       >
         已交（￥{amountTransform(deposit?.payAmount, '/')}）  于 {deposit?.payTime} 缴纳
-        <div style={{ color: 'red' }}>已实际完成店铺保证金退款操作，确认无误后登记如下：</div>
+        <div style={{ color: 'red' }}>{`${data?.type === 1 ? '已实际完成店铺保证金退款操作，确认无误后登记如下：' :'确认无误后线上原路退回如下：'}`}</div>
       </Form.Item>
 
       <Form.Item
         name="refendAmount"
-        label="请输入已退回金额"
-        rules={[{ required: true, message: '请输入已退回金额' }]}
+        label={`请输入${data?.type === 1 ? '已' : '要'}退回金额`}
+        rules={[{ required: true, message: `请输入${data?.type === 1 ? '已' : '要'}退回金额` }]}
       >
-        <Input placeholder="请输入已退回金额" suffix="元" />
+        <Input placeholder={`请输入${data?.type === 1 ? '已' : '要'}退回金额`} suffix="元" />
       </Form.Item>
 
       <ProFormTextArea
         name="remark"
         label="备注"
-        placeholder="请输入备注信息 6-50个字 例如：退款时间，退款操作人，退款打款银行卡号码等等"
+        placeholder={`请输入备注信息 6-50个字 ${data?.type === 1 ? '例如：退款时间，退款操作人，退款打款银行卡号码等等' : ''}`}
         rules={[{ required: true, message: '请输入备注' }]}
       />
 
