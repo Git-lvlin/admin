d680d5354988a7ac93ba201f46102708a58fc25b
coderby <coderby@yongdaxing.cn>
2021-08-06 17:33:12 +0800

    fixBug

--- src/pages/order-management/after-sales-order/after-sales-order.jsx
+++ src/pages/order-management/after-sales-order/after-sales-order.jsx
@@ -9,8 +9,8 @@ import { history } from 'umi'
 
 import { refundOrder } from '@/services/order-management/after-sales-order'
 import { amountTransform } from '@/utils/utils'
-// import Export from '@/pages/export-excel/export'
-// import ExportHistory from '@/pages/export-excel/export-history'
+import Export from '@/pages/export-excel/export'
+import ExportHistory from '@/pages/export-excel/export-history'
 
 
 const sourceType = {
@@ -152,8 +152,15 @@ const afterSalesOrder = () => {
               }}
             >
               {resetText}
-            </Button>
-          ]
+            </Button>,
+            <Export
+              change={(e)=> {setVisit(e)}}
+              key="export" 
+              type="order-common-export"
+              conditions={form?.getFieldValue()}
+            />,
+            <ExportHistory key="exportHistory" show={visit} setShow={setVisit}/>
+          ],
         }}
         headerTitle="数据列表"
         columns={columns}
