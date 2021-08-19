4efa463bb0d0f05cf1e1331b3a3092c1f4cc1cc9
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2021-08-19 15:20:12 +0800

    社区举报样式调整

--- src/pages/community-management/invitation-report/invitation-report.jsx
+++ src/pages/community-management/invitation-report/invitation-report.jsx
@@ -50,6 +50,7 @@ export default props => {
         dataIndex: 'content',
         valueType: 'text',
         hideInSearch: true,
+        ellipsis:true
     },
     {
         title: '被举报次数',
@@ -73,7 +74,8 @@ export default props => {
         valueEnum: {
             0: '正常',
             1: '已删除',
-        }
+        },
+        hideInSearch: true,
     },
     {
       title: '操作',
@@ -129,6 +131,7 @@ export default props => {
         dataIndex: 'content',
         valueType: 'text',
         hideInSearch: true,
+        ellipsis:true
     },
     {
         title: '被举报次数',
