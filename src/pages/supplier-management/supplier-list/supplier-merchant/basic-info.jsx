0c0ff2dfff3063d067d62ff9ddd8d5a1df2e0f18
zengweigang <zengweigang@yongdaxing.cn>
2021-08-19 16:32:31 +0800

    fix bug

--- src/pages/supplier-management/supplier-list/supplier-merchant/basic-info.jsx
+++ src/pages/supplier-management/supplier-list/supplier-merchant/basic-info.jsx
@@ -371,27 +371,27 @@ export default (props) => {
             required
             options={[
               {
-                label: '0%',
+                label: '0',
                 value: 0
               },
               {
-                label: '1%',
+                label: '1',
                 value: 1
               },
               {
-                label: '3%',
+                label: '3',
                 value: 3
               },
               {
-                label: '6%',
+                label: '6',
                 value: 6
               },
               {
-                label: '9%',
+                label: '9',
                 value: 9
               },
               {
-                label: '13%',
+                label: '13',
                 value: 13
               }
             ]}
