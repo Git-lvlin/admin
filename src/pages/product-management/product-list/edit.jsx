9810f6637ce98b6a4b114255dd54a42f739d9b51
huyueke <huyueke@yongdaxing.cn>
2021-08-20 11:34:56 +0800

    fix

--- src/pages/product-management/product-list/edit.jsx
+++ src/pages/product-management/product-list/edit.jsx
@@ -426,7 +426,7 @@ export default (props) => {
                 if (detailData) {
                   setLookVisible(true)
                 } else {
-                  message.error('请编辑完成后预览')
+                  message.error('请上传图片后预览')
                 }
               }}
             >
