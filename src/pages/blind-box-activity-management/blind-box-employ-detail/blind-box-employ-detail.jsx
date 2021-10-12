e576fd3111372803530eb7edd4c8330ce19163d8
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2021-10-12 18:21:08 +0800

    盲盒明细获取奖品样式修改

--- src/pages/blind-box-activity-management/blind-box-employ-detail/blind-box-employ-detail.jsx
+++ src/pages/blind-box-activity-management/blind-box-employ-detail/blind-box-employ-detail.jsx
@@ -85,7 +85,7 @@ const EmployDetail=(props) => {
         title: '获得奖品',
         dataIndex: 'prizeInfo',
         valueType: 'text',
-        width:230,
+        width:280,
         hideInSearch: true,
         render: (_, data)=>{
           if(data.type==5||data.type==6){
@@ -94,7 +94,7 @@ const EmployDetail=(props) => {
           if(data.prizeInfo?.prizeStatus==0){
             return <p>未抽中</p>
           }
-          return <div style={{display:'flex'}}>
+          return <div style={{display:'flex',justifyContent:'center'}}>
                     <Image src={data.prizeInfo.imageUrl} alt="" width='50px' height='50px' />
                     <div style={{marginLeft:'10px'}}>
                       <h5>{data.prizeInfo.goodsName}</h5>
