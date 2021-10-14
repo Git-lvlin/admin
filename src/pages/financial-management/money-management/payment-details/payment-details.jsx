5b81d63a0f8d50680d94e0b03fb7577c9f3a9e63
coderby <coderby@yongdaxing.cn>
2021-10-14 10:52:10 +0800

    增加财务管理订单类型

--- src/pages/financial-management/money-management/payment-details/payment-details.jsx
+++ src/pages/financial-management/money-management/payment-details/payment-details.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { PageContainer } from '@ant-design/pro-layout'
 import ProTable from '@ant-design/pro-table'
-import { useLocation } from "umi"
+import { useLocation, history } from "umi"
 
 import { logPage } from '@/services/financial-management/yeahgo-virtual-account-management'
 import { amountTransform } from '@/utils/utils'
@@ -9,6 +9,27 @@ import { tradeType } from '../../common-enum'
 
 const PaymentDetails = () => {
   const {query} = useLocation()
+
+  const skipToOrder = (id, type)=> {
+    switch(type) {
+      case 'normalOrder':
+      case 'second':
+      case 'dropShipping1688':
+      case 'signIn':
+      case 'blindBox':
+        history.push(`/order-management/normal-order-detail/${id}`)
+      break
+      case 'commandSalesOrder':
+      case 'activeSalesOrder':
+      case 'commandCollect':
+      case 'activeCollect':
+        history.push(`/order-management/intensive-order/supplier-order-detail/${id}`)
+      break
+      default:
+        return ''
+    }
+  }
+
   const columns = [
     {
       title: '序号',
@@ -50,6 +71,11 @@ const PaymentDetails = () => {
     {
       title: '订单号',
       dataIndex:'billNo',
+      render: (_, records)=> (
+        records.orderId ? 
+        <a onClick={()=>skipToOrder(records.orderId, records.orderType)}>{_}</a>:
+        <span>{_}</span>
+      )
     },
     {
       title: '平台单号',
