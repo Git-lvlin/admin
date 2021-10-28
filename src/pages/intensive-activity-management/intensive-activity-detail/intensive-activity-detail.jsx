355198643cc50e4990bd2b8f9ce2ed41cf30602d
zengweigang <zengweigang@yongdaxing.cn>
2021-10-28 10:41:32 +0800

    shareImg

--- src/pages/intensive-activity-management/intensive-activity-detail/intensive-activity-detail.jsx
+++ src/pages/intensive-activity-management/intensive-activity-detail/intensive-activity-detail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Spin, Descriptions, Divider, Table, Row, Typography } from 'antd';
+import { Spin, Descriptions, Divider, Table, Row, Typography, Image } from 'antd';
 import { amountTransform } from '@/utils/utils'
 import { useParams } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
@@ -96,7 +96,7 @@ const Detail = () => {
       if (res.code === 0) {
         setDetailData(res.data);
       }
-    }).finally(()=>{
+    }).finally(() => {
       setLoading(false);
     })
   }
@@ -132,6 +132,14 @@ const Detail = () => {
               <Descriptions.Item label="活动创建人">
                 {detailData?.wholesale?.createAdminName}
               </Descriptions.Item>
+              {detailData?.wholesale?.shareImg &&
+                <Descriptions.Item label="分享海报">
+                  <Image src={detailData?.wholesale?.shareImg} width={100} />
+                </Descriptions.Item>
+              }
+              <Descriptions.Item label="配送模式">
+                {detailData?.wholesale?.wholesaleFlowTypeDesc}
+              </Descriptions.Item>
               <Descriptions.Item label="创建时间">
                 {detailData?.wholesale?.createTime}
               </Descriptions.Item>
@@ -150,7 +158,7 @@ const Detail = () => {
           </Row>
         </div>
       </Spin>
-      
+
     </PageContainer>
   );
 };
