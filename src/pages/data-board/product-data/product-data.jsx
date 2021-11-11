a89714b465590327e8b819bff9efd03c9cc5bf59
coderby <coderby@yongdaxing.cn>
2021-11-11 11:29:37 +0800

    增加商品明细数据

--- src/pages/data-board/product-data/product-data.jsx
+++ src/pages/data-board/product-data/product-data.jsx
@@ -9,7 +9,7 @@ import TableSearch from './table-search'
 import { getTimeDistance } from '@/utils/utils'
 import styles from './styles.less'
 import GcCascader from '@/components/gc-cascader'
-import { timeGoodType } from '@/services/data-board/product-data'
+import { timeGoodType, goodDetail } from '@/services/data-board/product-data'
 import { amountTransform } from '@/utils/utils'
 
 const ProductData = () => {
@@ -109,84 +109,92 @@ const ProductData = () => {
   const goodsDetail = [
     {
       title: '商品编码',
-      dataIndex: 'a',
+      dataIndex: 'spuId',
       align: 'center'
     },
     {
       title: '商品名称',
-      dataIndex: 'd',
-      align: 'center'
+      dataIndex: 'goodsName',
+      align: 'center',
+      width: '15%'
     },
     {
       title: '规格',
-      dataIndex: 'f',
+      dataIndex: 'skuName',
       align: 'center',
       hideInSearch: true
     },
     {
       title: '商品复购率',
-      dataIndex: 'g',
+      dataIndex: 'againUCount',
       align: 'center',
       hideInSearch: true
     },
     {
       title: '支付商品数',
-      dataIndex: 'h',
+      dataIndex: 'gCount',
       align: 'center',
       hideInSearch: true
     },
     {
       title: '商品支付总金额',
-      dataIndex: 'j',
+      dataIndex: 'payAmount',
       align: 'center',
-      hideInSearch: true
+      hideInSearch: true,
+      render: (_) => `￥${amountTransform(Number(_), '/')}`
     },
     {
       title: '支付用户数',
-      dataIndex: 'k',
+      dataIndex: 'uCount',
       align: 'center',
       hideInSearch: true
     },
     {
       title: '商品退款数',
-      dataIndex: 'v',
+      dataIndex: 'refundPayCount',
       align: 'center',
       hideInSearch: true
     },
     {
       title: '商品退款总金额',
-      dataIndex: 'x',
+      dataIndex: 'refundAmount',
       align: 'center',
-      hideInSearch: true
+      hideInSearch: true,
+      render: (_) => `￥${amountTransform(Number(_), '/')}`
     },
     {
       title: '商品退款率',
-      dataIndex: 'z',
+      dataIndex: 'refundPayCount',
       align: 'center',
       hideInSearch: true
     },
     {
       title: '商品类型',
-      dataIndex: 'l',
+      dataIndex: 'orderType',
       valueType: 'select',
       valueEnum: {
-        0: '秒约商品',
-        1: '集约商品',
-        2: '代发商品'
+        "2": '秒约商品',
+        "15": '集约商品',
+        "11": '代发商品'
       },
       align: 'center',
+      initialValue: "2",
+      fieldProps:{
+        allowClear: false
+      },
       hideInTable: true
     },
     {
       title: '统计时间范围',
-      dataIndex: 'i',
-      valueType: 'dateTimeRange',
+      dataIndex: 'date',
+      valueType: 'dateRange',
       align: 'center',
+      initialValue: getTimeDistance("week"),
       hideInTable: true
     },
     {
       title: '商品分类',
-      dataIndex: 'e',
+      dataIndex: 'gcId',
       renderFormItem: () => (<GcCascader />),
       align: 'center',
       hideInTable: true
@@ -218,8 +226,10 @@ const ProductData = () => {
         </ProCard>
       </ProCard>
       <ProTable
-        rowKey=""
+        rowKey="spuId"
         columns={goodsDetail}
+        params={{}}
+        request={goodDetail}
         pagination={{
           showQuickJumper: true,
           pageSize: 10
