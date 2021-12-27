46da9bbd7e37989bc4ecae68f08b16863bb62a24
coderby <coderby@yongdaxing.cn>
2021-12-27 09:56:05 +0800

    fixBug

--- src/pages/financial-management/transaction-detail-management/loan-detail-management/loan-detail-management.jsx
+++ src/pages/financial-management/transaction-detail-management/loan-detail-management/loan-detail-management.jsx
@@ -67,7 +67,13 @@ const LoanDetailManagement = () =>{
       title: '订单类型',
       dataIndex: 'orderType',
       valueType: 'select',
-      valueEnum: orderType
+      valueEnum: orderType,
+      hideInTable: true
+    },
+    {
+      title: '订单类型',
+      dataIndex: 'orderTypeDesc',
+      hideInSearch: true
     },
     {
       title: '订单号',
