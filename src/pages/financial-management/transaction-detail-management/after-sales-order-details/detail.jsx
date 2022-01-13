f71bfc7c27603f5966419845b81adc9750825a2d
simon <zhangjian@yongdaxing.cn>
2022-01-13 15:14:53 +0800

    修改订单支付明细字段

--- src/pages/financial-management/transaction-detail-management/after-sales-order-details/detail.jsx
+++ src/pages/financial-management/transaction-detail-management/after-sales-order-details/detail.jsx
@@ -146,7 +146,7 @@ const Detail = () => {
         'second': '秒约',
         'commandSalesOrder': '集约批发订单',
         'dropShipping1688': '1688代发订单',
-        'commandCollect': '集约销售订单',
+        'commandCollect': '集约零售订单',
         'blindBox': '盲盒订单',
         'signIn': '签到订单'
       }
