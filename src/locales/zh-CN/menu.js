2bd0ed28423716cd28f9991c105f3aba7f5b32bf
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2022-01-20 15:22:33 +0800

    新春盖楼活动解决key问题

--- src/locales/zh-CN/menu.js
+++ src/locales/zh-CN/menu.js
@@ -335,6 +335,7 @@ export default {
   'menu.activity-management.spring-festival-build-building-activity.consume-detail': '使用明细',
   'menu.activity-management.spring-festival-build-building-activity.grant-detail': '发放明细',
   'menu.activity-management.spring-festival-build-building-activity.cash-advance-detail': '提现明细',
+  'menu.activity-management.spring-festival-build-building-activity.employ-detail': '查看用户明细',
   'menu.activity-management.spring-festival-build-building-activity.ranking-data': '排名数据',
   'menu.activity-management.spring-festival-build-building-activity.binding-set': '提现绑定设置',
 
