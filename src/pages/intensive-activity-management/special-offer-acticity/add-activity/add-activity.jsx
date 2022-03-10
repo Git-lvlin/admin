960481ef80a59cb8ede667d477cabe9c3a9b8699
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2022-03-10 10:58:32 +0800

    一分钱活动新建跳转调整

--- src/pages/intensive-activity-management/special-offer-acticity/add-activity/add-activity.jsx
+++ src/pages/intensive-activity-management/special-offer-acticity/add-activity/add-activity.jsx
@@ -103,7 +103,7 @@ export default (props) => {
       saveWSDiscountActiveConfig(parmas).then(res=>{
         if(res.code==0){
           message.success(id?'编辑成功':'添加成功'); 
-          history.push('/intensive-activity-management/special-offer-acticity/special-offer-acticity-list')
+          window.location.href='/intensive-activity-management/special-offer-acticity/special-offer-acticity-list'
         }
       })
     } catch (error) {
