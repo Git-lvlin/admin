fa361cc2887ca871a2f9ec9bdd9b7e13314a15a0
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2022-04-06 10:46:39 +0800

    活动数据修改

--- src/pages/intensive-activity-management/special-offer-acticity/special-offer-acticity-list/activity-data.jsx
+++ src/pages/intensive-activity-management/special-offer-acticity/special-offer-acticity-list/activity-data.jsx
@@ -109,7 +109,9 @@ export default (props) => {
       activityCode:'wsDiscountActiveCode'
     }
     activityData(params).then(res=>{
-      setDetailList(res.data[0])
+      if(res.code==0){
+        setDetailList(res.data[0])
+      }
     })
 
   }, [time])
