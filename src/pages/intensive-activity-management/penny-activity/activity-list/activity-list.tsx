20e83fa380c177890e3156b8128e68c8fb2136ad
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2022-04-06 18:56:51 +0800

    修改冲突

--- src/pages/intensive-activity-management/penny-activity/activity-list/activity-list.tsx
+++ src/pages/intensive-activity-management/penny-activity/activity-list/activity-list.tsx
@@ -3,7 +3,7 @@ import { Button} from 'antd';
 import ProTable from '@ant-design/pro-table';
 import { PlusOutlined } from '@ant-design/icons';
 import { getActiveConfigList} from '@/services/intensive-activity-management/penny-activity';
-import { PageContainer } from '@ant-design/pro-layout';
+import { PageContainer } from '@/components/PageContainer';
 import moment from 'moment'
 import EndModel from './end-model'
 import AddedActivity from '../added-activity'
