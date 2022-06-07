4644f407b009d9dbed9826f605acf9a0b6eb58e9
coderby <wubinyan@qhhznt.com>
2022-06-07 17:24:58 +0800

    增加氢原子业绩报表

---
+++ src/pages/operation-management/performance-report/data.d.ts
@@ -0,0 +1,14 @@
+export type TableProps = {
+  operationId: string;
+  operationName: string;
+  totalCommission: number;
+  totalSaleCommission: number;
+  totalRentCommission: number;
+}
+
+export type DetailDrawerProps = {
+  visible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  type: number;
+  id?: string;
+}
