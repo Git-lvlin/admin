7aa55c6866f824e02658eff213d5fd16564ecbe3
coderby <wubinyan@qhhznt.com>
2022-06-22 17:57:52 +0800

    完成数据埋点

--- src/pages/buried-point/incident-analysts/index.tsx
+++ src/pages/buried-point/incident-analysts/index.tsx
@@ -1,53 +1,61 @@
+import { useState } from "react"
 import { PageContainer } from "@ant-design/pro-layout"
 import ProTable from "@ant-design/pro-table"
+import moment from "moment"
 
 import type { FC } from "react"
 import type { ProColumns } from "@ant-design/pro-table"
 import type { ProTableProps } from "../data"
 
 import SearchModal from "./search-modal"
+import { indexDetail } from "@/services/buried-point/incident-analysts"
+import SelectDate from "./select-date"
 
 const IncidentAnalysts: FC = () => {
+  const [ formData, setFormData ] = useState({})
+  const [ title, setTitle ] = useState<string>('')
+  const [ rangePicker, setRangePicker ] = useState<moment.Moment[]>([])
 
   const columns: ProColumns<ProTableProps>[] = [
-    // {
-    //   title: '',
-    //   dataIndex: '',
-    //   align: 'center'
-    // },
-    // {
-    //   title: '',
-    //   dataIndex: '',
-    //   align: 'center'
-    // },
-    // {
-    //   title: '',
-    //   dataIndex: '',
-    //   align: 'center'
-    // },
-    // {
-    //   title: '',
-    //   dataIndex: '',
-    //   align: 'center'
-    // },
-    // {
-    //   title: '',
-    //   dataIndex: '',
-    //   align: 'center'
-    // }
+    {
+      title: '日期',
+      dataIndex: 'dates',
+      align: 'center'
+    },
+    {
+      title: `${title}总次数`,
+      dataIndex: 'totalNum',
+      align: 'center'
+    },
+    {
+      title: `${title}总人数`,
+      dataIndex: 'userNum',
+      align: 'center'
+    },
+    {
+      title: `${title}平均次数`,
+      dataIndex: 'average',
+      align: 'center'
+    }
   ]
 
   return (
     <PageContainer title={false}>
-      <SearchModal/>
+      <SearchModal setFormData={setFormData} setTitle={setTitle}/>
       <ProTable
-        rowKey=''
+        rowKey='dates'
         columns={columns}
-        params={{}}
+        request={indexDetail}
+        params={{
+          ...formData, 
+          startTime: rangePicker[0]?.format('YYYY-MM-DD'),
+          endTime: rangePicker[1]?.format('YYYY-MM-DD')
+        }}
         pagination={{
           showQuickJumper: true,
           pageSize: 10
         }}
+        headerTitle={<SelectDate setRangePickerValue={setRangePicker}/>}
         options={false}
         search={false}
       />
