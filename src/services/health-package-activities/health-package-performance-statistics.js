dd5c87730cf4e8a7c98a2a937a8746ebb261a4e3
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2022-12-27 15:39:08 +0800

    fix

--- src/services/health-package-activities/health-package-performance-statistics.js
+++ src/services/health-package-activities/health-package-performance-statistics.js
@@ -1,4 +1,5 @@
 import request from '@/utils/request'
+import { amountTransform } from '@/utils/utils'
 
 export const cardCityAgencyOrderPm = async (params = {}, options = {}) => {
   const { current = 1, pageSize = 10, area, payAmount, orderNums, ...rest } = params
@@ -10,8 +11,8 @@ export const cardCityAgencyOrderPm = async (params = {}, options = {}) => {
       provinceId: area&&area[0]?.value,
       cityId: area&&area[1]?.value,
       districtId: area&&area[2]?.value,
-      minPayAmount: payAmount&&payAmount?.min,
-      maxPayAmount: payAmount&&payAmount?.max,
+      minPayAmount: payAmount&& amountTransform(payAmount?.min,'*'),
+      maxPayAmount: payAmount&& amountTransform(payAmount?.max,'*'),
       minOrderNums: orderNums&&orderNums?.min,
       maxOrderNums: orderNums&&orderNums?.max,
       ...rest
@@ -34,8 +35,8 @@ export const cardCityAgencyOrderPmStats = async (params = {}, options = {}) => {
         provinceId: area&&area[0]?.value,
         cityId: area&&area[1]?.value,
         districtId: area&&area[2]?.value,
-        minPayAmount: payAmount&&payAmount?.min,
-        maxPayAmount: payAmount&&payAmount?.max,
+        minPayAmount: payAmount&& amountTransform(payAmount?.min,'*'),
+        maxPayAmount: payAmount&& amountTransform(payAmount?.max,'*'),
         minOrderNums: orderNums&&orderNums?.min,
         maxOrderNums: orderNums&&orderNums?.max,
         ...rest
