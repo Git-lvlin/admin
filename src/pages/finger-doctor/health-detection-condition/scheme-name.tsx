6bb50dd677be4403afa60bab474d617624b3e8b5
coderby <wubinyan@qhhznt.com>
2023-03-31 14:55:56 +0800

    fixBug

--- src/pages/finger-doctor/health-detection-condition/scheme-name.tsx
+++ src/pages/finger-doctor/health-detection-condition/scheme-name.tsx
@@ -3,9 +3,7 @@ import {
   ModalForm,
   ProFormText,
   ProFormTextArea,
-  ProFormCheckbox,
   ProFormDigit,
-  ProFormDependency,
 } from '@ant-design/pro-form'
 
 import type { FC } from 'react'
@@ -105,42 +103,6 @@ const SchemeName: FC<schemeNameProps> = ({visible, setVisible, title, index, for
         ]}
         width='md'
       />
-      <ProFormCheckbox.Group
-        label='是否要短信推送'
-        name='isSms'
-        options={[{label:'需要推送短信通知', value: true}]}
-      />
-      <ProFormDependency name={['isSms']}>
-        {
-          ({isSms})=> {
-            if(isSms && isSms[0]) {
-              return (
-                <ProFormTextArea
-                  rules={[
-                    () => ({
-                      validator(_, value) {
-                        if (value&&value.length < 8) {
-                          return Promise.reject(new Error('不少于8个字符'))
-                        }
-                        return Promise.resolve()
-                      }
-                    })
-                  ]}
-                  label='短信内容'
-                  name='smsContent'
-                  fieldProps={{
-                    maxLength: 50,
-                    minLength: 8,
-                    placeholder: '请输入8-50个字'
-                  }}
-                />
-              )
-            } else {
-              return
-            }
-          }
-        }
-      </ProFormDependency>
       <ProFormDigit
         label='推送消息时间'
         name='time'
