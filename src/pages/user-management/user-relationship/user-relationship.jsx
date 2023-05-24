3f70a8c97ef570c092ee6555ded05feef7861664
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2023-05-24 17:12:24 +0800

    fix

--- src/pages/user-management/user-relationship/user-relationship.jsx
+++ src/pages/user-management/user-relationship/user-relationship.jsx
@@ -65,7 +65,7 @@ const UserRelationship = () => {
     return {
       ...form.current?.getFieldsValue(),
       phoneNumber: phoneNumber,
-      memberId: memberId
+      pid: memberId
     }
   }
 
