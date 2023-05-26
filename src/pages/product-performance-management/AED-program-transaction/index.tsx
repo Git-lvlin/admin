1639e4f18391099b8b1832466af004db9603a607
coderby <wubinyan@qhhznt.com>
2023-05-26 17:55:32 +0800

    fixBug

--- src/pages/product-performance-management/AED-program-transaction/index.tsx
+++ src/pages/product-performance-management/AED-program-transaction/index.tsx
@@ -145,241 +145,235 @@ const AEDTable: React.FC<{search?: FormInstance<any> | any, change: number}> = (
 
   return (
     <div>
-      <div className={styles.mTable}>
-        <ProForm
-          layout='vertical'
-          formRef={form}
-          submitter={false}
-        >
-          <Space>
-            <div>明细汇总说明：线上订单汇总统计数据根据筛选结果得来，与下面明细数据的合计一致；线下订单数据由管理员手工编辑录入，查询后线下数据置0。</div>
-            <a onClick={()=>{setExportVisible(true)}}>查看历史录入记录</a>
-          </Space>
-          <h2>AED销售明细汇总</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>业务项</th>
-                <th colSpan={2}>单独SPU保证金</th>
-                <th colSpan={5 }>销售课程数</th>
-                <th colSpan={6 }>课程的保证金</th>
-                <th colSpan={4}>保证金的合同：合同签署情况</th>
-              </tr> 
-            </thead>
-            <tbody>
-              <tr>
-                <th>订单项</th>
-                <th>线上已交10000元保证金订单</th>
-                <th>线下已交10000元保证金订单</th>
-                <th>旧3800订单</th>
-                <th>线上 5&3800&13800&14300&4300订单</th>
-                <th>线下 5&3800&13800&14300&4300订单</th>
-                <th>线上4800区县订单</th>
-                <th>线下4800区县订单</th>
-                <th>课程订单总计</th>
-                <th>线上已交保证金订单</th>
-                <th>线下已交保证金订单</th>
-                <th>线上需交未交保证金订单</th>
-                <th>线下需交未交保证金订单</th>
-                <th>线上无需交保证金订单</th>
-                <th>线下无需交保证金订单</th>
-                <th>线下已签纸质合同数</th>
-                <th>线上已签电子合同数</th>
-                <th>已签合同数</th>
-                <th>未签合同数 </th>
-              </tr>
-              <tr>
-                <th>订单数量</th>
-                {
-                  orderNum1.map(res => (
-                    <td key={res.name}>
-                      {
-                        res.type ?
-                        <ProFormText 
-                          name={res.name} 
-                          readonly
-                        />:
-                        <ProFormDigit 
-                          name={res.name} 
-                          fieldProps={{
-                            bordered: false,
-                            controls: false
-                          }}
-                        />
-                      }
-                    </td>
-                  ))
-                }
-                <td>
-                  <ProFormDependency name={['onlineCourseNum', 'offlineCourseNum', 'onlineDcNum', 'offlineDcNum']}>
-                    {
-                      ({onlineCourseNum, offlineCourseNum, onlineDcNum, offlineDcNum, oteOrderNum}) => (
-                        <div>{numberTransform(onlineCourseNum) + numberTransform(offlineCourseNum) + numberTransform(onlineDcNum) + numberTransform(offlineDcNum) + numberTransform(oteOrderNum)}</div>
-                      )
-                    }
-                  </ProFormDependency>
-                </td>
-                {
-                  orderNum2.map(res => (
-                    <td key={res.name}>
-                      {
-                        res.type ?
-                        <ProFormText 
-                          name={res.name} 
-                          readonly
-                        />:
-                        <ProFormDigit 
-                          name={res.name} 
-                          fieldProps={{
-                            bordered: false,
-                            controls: false
-                          }}
-                        />
-                      }
-                    </td>
-                  ))
-                }
-                <td>
-                  <ProFormDependency name={['offlineContractNum', 'onlineContractNum']}>
-                    {
-                      ({offlineContractNum, onlineContractNum}) => (
-                        <div>{numberTransform(offlineContractNum) + numberTransform(onlineContractNum)}</div>
-                      )
-                    }
-                  </ProFormDependency>
-                </td>
-                <td>
-                  <ProFormText
-                    name='noSignContractNum'
-                    readonly
-                  />
-                </td>
-              </tr>
-              <tr>
-                <th>小计金额</th>
-                <td>
-                  <ProFormText 
-                    name='onlineDopSimpleAmount'
-                    readonly
-                  />
-                </td>
-                <td>
-                  <ProFormDependency name={['offlineDopSimpleNum']}>
-                    {
-                      ({offlineDopSimpleNum}) => <div>{offlineDopSimpleNum ? offlineDopSimpleNum * 10000 : '-'}</div>
-                    }
-                  </ProFormDependency>
-                </td>
-                <td>
-                  <ProFormText 
-                    name='oteAmount'
-                    readonly
-                  />
-                </td>
-                <td>
-                  <ProFormText 
-                    name='onlineCourseAmount'
-                    readonly
-                  />
-                </td>
-                <td>
-                  <ProFormDigit
-                    name='offlineCourseAmount'
-                    fieldProps={{
-                      bordered: false,
-                      controls: false
-                    }}
-                  />
-                </td>
-                <td>
-                  <ProFormText 
-                    name='onlineDcAmount'
-                    readonly
-                  />
-                </td>
-                <td>
-                  <ProFormDependency name={['offlineDcNum']}>
-                    {
-                      ({offlineDcNum}) => (
-                        <div>{offlineDcNum ? offlineDcNum * 4800 : '-'}</div>
-                      )
-                    }
-                  </ProFormDependency>
-                </td>
-                <td>
-                  <ProFormDependency name={['onlineCourseAmount', 'offlineCourseAmount', 'onlineDcAmount', 'offlineDcNum', 'oteAmount']}>
-                    {
-                      ({onlineCourseAmount, offlineCourseAmount, onlineDcAmount, offlineDcNum, oteAmount}) => (
-                        <div>{numberTransform(onlineCourseAmount) + numberTransform(offlineCourseAmount) + numberTransform(onlineDcAmount) + (numberTransform(offlineDcNum) *4800) + numberTransform(oteAmount)}</div>
-                      )
-                    }
-                  </ProFormDependency>
-                </td>
-                {
-                  subTotal.map(res => (
-                    <td key={res.name}>
-                      {
-                        res.type ?
-                        <ProFormText 
-                          name={res.name} 
-                          readonly
-                        />:
-                        <ProFormDigit 
-                          name={res.name} 
-                          fieldProps={{
-                            bordered: false,
-                            controls: false
-                          }}
-                        />
-                      }
-                    </td>
-                  ))
-                }
-              </tr>
-              <tr>
-                <th>总计金额</th>
-                <td colSpan={7}>
-                  <ProFormDependency name={['onlineDopSimpleAmount', 'offlineDopSimpleNum', 'onlineCourseAmount', 'offlineCourseAmount', 'onlineDcAmount', 'offlineDcNum', 'oteAmount']}>
-                    {
-                      ({onlineDopSimpleAmount, offlineDopSimpleNum, onlineCourseAmount, offlineCourseAmount, onlineDcAmount, offlineDcNum, oteAmount}) => (
-                        <div>{ numberTransform(onlineDopSimpleAmount) + numberTransform(offlineDopSimpleNum * 10000) + numberTransform(onlineCourseAmount) + numberTransform(offlineCourseAmount) + numberTransform(offlineDcNum * 4800) + numberTransform(onlineDcAmount) + numberTransform(oteAmount)}</div>
-                      )
-                    }
-                  </ProFormDependency>
-                </td>
-                {
-                  totalAmount.map(res => (
-                    <td key={res.name}>
-                      {
-                        res.type ?
-                        <ProFormText 
-                          name={res.name} 
-                          readonly
-                        />:
-                        <ProFormDigit 
-                          name={res.name} 
-                          fieldProps={{
-                            bordered: false,
-                            controls: false
-                          }}
-                        />
-                      }
-                    </td>
-                  ))
-                }
-              </tr>
-            </tbody>
-          </table>
-        </ProForm>
+      <Space>
+        <div>明细汇总说明：线上订单汇总统计数据根据筛选结果得来，与下面明细数据的合计一致；线下订单数据由管理员手工编辑录入，查询后线下数据置0。</div>
+        <a onClick={()=>{setExportVisible(true)}}>查看历史录入记录</a>
+      </Space>
+      <h2>AED销售明细汇总</h2>
+      {/* <div className={styles.mTable}>
         
-        {
-          exportVisible &&
-          <ExportLog 
-            visible={exportVisible}
-            setVisible={setExportVisible}
-          />
-        }
-      </div>
+      </div> */}
+      <ProForm
+        className={styles.mTable}
+        layout='vertical'
+        formRef={form}
+        submitter={false}
+      >
+        <table>
+          <thead>
+            <tr>
+              <th>业务项</th>
+              <th colSpan={2}>单独SPU保证金</th>
+              <th colSpan={5 }>销售课程数</th>
+              <th colSpan={6 }>课程的保证金</th>
+              <th colSpan={4}>保证金的合同：合同签署情况</th>
+            </tr> 
+          </thead>
+          <tbody>
+            <tr>
+              <th>订单项</th>
+              <th>线上已交10000元保证金订单</th>
+              <th>线下已交10000元保证金订单</th>
+              <th>旧3800订单</th>
+              <th>线上 5&3800&13800&14300&4300订单</th>
+              <th>线下 5&3800&13800&14300&4300订单</th>
+              <th>线上4800区县订单</th>
+              <th>线下4800区县订单</th>
+              <th>课程订单总计</th>
+              <th>线上已交保证金订单</th>
+              <th>线下已交保证金订单</th>
+              <th>线上需交未交保证金订单</th>
+              <th>线下需交未交保证金订单</th>
+              <th>线上无需交保证金订单</th>
+              <th>线下无需交保证金订单</th>
+              <th>线下已签纸质合同数</th>
+              <th>线上已签电子合同数</th>
+              <th>已签合同数</th>
+              <th>未签合同数 </th>
+            </tr>
+            <tr>
+              <th>订单数量</th>
+              {
+                orderNum1.map(res => (
+                  <td key={res.name}>
+                    {
+                      res.type ?
+                      <ProFormText 
+                        name={res.name} 
+                        readonly
+                      />:
+                      <ProFormDigit 
+                        name={res.name} 
+                        fieldProps={{
+                          bordered: false,
+                          controls: false
+                        }}
+                      />
+                    }
+                  </td>
+                ))
+              }
+              <td>
+                <ProFormDependency name={['onlineCourseNum', 'offlineCourseNum', 'onlineDcNum', 'offlineDcNum']}>
+                  {
+                    ({onlineCourseNum, offlineCourseNum, onlineDcNum, offlineDcNum, oteOrderNum}) => (
+                      <div>{numberTransform(onlineCourseNum) + numberTransform(offlineCourseNum) + numberTransform(onlineDcNum) + numberTransform(offlineDcNum) + numberTransform(oteOrderNum)}</div>
+                    )
+                  }
+                </ProFormDependency>
+              </td>
+              {
+                orderNum2.map(res => (
+                  <td key={res.name}>
+                    {
+                      res.type ?
+                      <ProFormText 
+                        name={res.name} 
+                        readonly
+                      />:
+                      <ProFormDigit 
+                        name={res.name} 
+                        fieldProps={{
+                          bordered: false,
+                          controls: false
+                        }}
+                      />
+                    }
+                  </td>
+                ))
+              }
+              <td>
+                <ProFormDependency name={['offlineContractNum', 'onlineContractNum']}>
+                  {
+                    ({offlineContractNum, onlineContractNum}) => (
+                      <div>{numberTransform(offlineContractNum) + numberTransform(onlineContractNum)}</div>
+                    )
+                  }
+                </ProFormDependency>
+              </td>
+              <td>
+                <ProFormText
+                  name='noSignContractNum'
+                  readonly
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>小计金额</th>
+              <td>
+                <ProFormText 
+                  name='onlineDopSimpleAmount'
+                  readonly
+                />
+              </td>
+              <td>
+                <ProFormDependency name={['offlineDopSimpleNum']}>
+                  {
+                    ({offlineDopSimpleNum}) => <div>{offlineDopSimpleNum ? offlineDopSimpleNum * 10000 : '-'}</div>
+                  }
+                </ProFormDependency>
+              </td>
+              <td>
+                <ProFormText 
+                  name='oteAmount'
+                  readonly
+                />
+              </td>
+              <td>
+                <ProFormText 
+                  name='onlineCourseAmount'
+                  readonly
+                />
+              </td>
+              <td>
+                <ProFormDigit
+                  name='offlineCourseAmount'
+                  fieldProps={{
+                    bordered: false,
+                    controls: false
+                  }}
+                />
+              </td>
+              <td>
+                <ProFormText 
+                  name='onlineDcAmount'
+                  readonly
+                />
+              </td>
+              <td>
+                <ProFormDependency name={['offlineDcNum']}>
+                  {
+                    ({offlineDcNum}) => (
+                      <div>{offlineDcNum ? offlineDcNum * 4800 : '-'}</div>
+                    )
+                  }
+                </ProFormDependency>
+              </td>
+              <td>
+                <ProFormDependency name={['onlineCourseAmount', 'offlineCourseAmount', 'onlineDcAmount', 'offlineDcNum', 'oteAmount']}>
+                  {
+                    ({onlineCourseAmount, offlineCourseAmount, onlineDcAmount, offlineDcNum, oteAmount}) => (
+                      <div>{numberTransform(onlineCourseAmount) + numberTransform(offlineCourseAmount) + numberTransform(onlineDcAmount) + (numberTransform(offlineDcNum) *4800) + numberTransform(oteAmount)}</div>
+                    )
+                  }
+                </ProFormDependency>
+              </td>
+              {
+                subTotal.map(res => (
+                  <td key={res.name}>
+                    {
+                      res.type ?
+                      <ProFormText 
+                        name={res.name} 
+                        readonly
+                      />:
+                      <ProFormDigit 
+                        name={res.name} 
+                        fieldProps={{
+                          bordered: false,
+                          controls: false
+                        }}
+                      />
+                    }
+                  </td>
+                ))
+              }
+            </tr>
+            <tr>
+              <th>总计金额</th>
+              <td colSpan={7}>
+                <ProFormDependency name={['onlineDopSimpleAmount', 'offlineDopSimpleNum', 'onlineCourseAmount', 'offlineCourseAmount', 'onlineDcAmount', 'offlineDcNum', 'oteAmount']}>
+                  {
+                    ({onlineDopSimpleAmount, offlineDopSimpleNum, onlineCourseAmount, offlineCourseAmount, onlineDcAmount, offlineDcNum, oteAmount}) => (
+                      <div>{ numberTransform(onlineDopSimpleAmount) + numberTransform(offlineDopSimpleNum * 10000) + numberTransform(onlineCourseAmount) + numberTransform(offlineCourseAmount) + numberTransform(offlineDcNum * 4800) + numberTransform(onlineDcAmount) + numberTransform(oteAmount)}</div>
+                    )
+                  }
+                </ProFormDependency>
+              </td>
+              {
+                totalAmount.map(res => (
+                  <td key={res.name}>
+                    {
+                      res.type ?
+                      <ProFormText 
+                        name={res.name} 
+                        readonly
+                      />:
+                      <ProFormDigit 
+                        name={res.name} 
+                        fieldProps={{
+                          bordered: false,
+                          controls: false
+                        }}
+                      />
+                    }
+                  </td>
+                ))
+              }
+            </tr>
+          </tbody>
+        </table>
+      </ProForm>
       <Space size='middle' className={styles.mTableSave}>
         <Button type='primary' onClick={()=> {submit(1)}} key="submit">
           确认保存
@@ -395,6 +389,13 @@ const AEDTable: React.FC<{search?: FormInstance<any> | any, change: number}> = (
         />
       </Space>
       <div className={styles.title}>线上订单数据明细</div>
+      {
+        exportVisible &&
+        <ExportLog 
+          visible={exportVisible}
+          setVisible={setExportVisible}
+        />
+      }
     </div>
   )
 }
