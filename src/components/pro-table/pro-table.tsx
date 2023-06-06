06ae9886e0873b5184f20099ffedc4bf5416c5a7
zengweigang <zengweigang@yongdaxing.cn>
2023-06-06 10:52:28 +0800

    save

---
+++ src/components/pro-table/pro-table.tsx
@@ -0,0 +1,28 @@
+import ProTable from '@ant-design/pro-table';
+import type { ProTableProps } from '@ant-design/pro-table';
+import type { TablePaginationConfig } from 'antd';
+
+interface I extends ProTableProps<Record<string, unknown>, Record<string, unknown>> {
+  paginationProps: false | TablePaginationConfig
+}
+
+const P = (props: I) => {
+  const { pagination, paginationProps = {}, ...rest } = props
+  return <ProTable
+    revalidateOnFocus={false}
+    pagination={{
+      pageSize: 10,
+      showQuickJumper: true,
+      hideOnSinglePage: false,
+      showSizeChanger: true,
+      ...paginationProps,
+    }}
+    {...rest}
+  />
+}
+
+P.Summary = ProTable.Summary;
+
+export default P
+
+
