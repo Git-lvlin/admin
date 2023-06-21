9e575ef1c96e12e937fc0157a33ab53f0e53430a
zengweigang <zengweigang@yongdaxing.cn>
2023-06-21 09:34:27 +0800

    fix

--- src/components/pro-table/pro-table.tsx
+++ src/components/pro-table/pro-table.tsx
@@ -16,7 +16,7 @@ const P = (props: I) => {
       hideOnSinglePage: false,
       showSizeChanger: true,
       ...paginationProps,
-    }}
+    }:false}
     scroll={{ x: 'max-content' }}
     {...rest}
   />
