e96d6e5c3bb976f4a4fe132226468e7300944e08
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2023-07-20 09:07:25 +0800

    fix

--- src/pages/aed-team-leader/team-leader-management/entering-information.tsx
+++ src/pages/aed-team-leader/team-leader-management/entering-information.tsx
@@ -149,7 +149,6 @@ export default (props:EnteringProps) => {
 
       <Form.Item
         label="地址"
-        rules={[{ required: true, message: '请选择省市区' }]}
         name="area"
       >
         <AddressCascader style={{ width: 250 }} />
