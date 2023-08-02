ac36d995d2296ed38bc87f35e32dd4307ed2a7c1
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2023-08-02 14:00:44 +0800

    save

---
+++ src/pages/product-sharing-management/transaction-sharing-management/allocation-management/update-history.tsx
@@ -0,0 +1,132 @@
+import { useState, useRef, useEffect } from "react"
+import ProTable from '@/components/pro-table'
+import type { ProColumns,ActionType } from "@ant-design/pro-table"
+import TimeSelect from '@/components/time-select'
+
+import { getLogListByParams } from "@/services/product-management/transaction-sharing-management"
+import {
+  DrawerForm
+} from '@ant-design/pro-form';
+// import AdvertisementDetails from './advertisement-details'
+import { CumulativeProps } from './data';
+
+export default (props:CumulativeProps) => {
+  const { visible, setVisible } = props;
+  const form = useRef<ActionType>()
+  const [detailVisible,setDetailVisible] = useState<boolean>(false)
+  const [parameter,setParameter] = useState()
+  const [positionSelect,setPositionSelect] = useState()
+  const [advSelect, setAdvSelect] = useState()
+
+  useEffect(()=>{
+    // advPositionPage().then(res=>{
+    //   console.log('res',res)
+    //   let obj={}
+    //   res.data?.map((item: { code: string | number; title: string })=>{
+    //     obj[item.code]=item.title
+    //   })
+    //   console.log('obj',obj)
+    //   setPositionSelect(obj)
+    // })
+  },[])
+
+  useEffect(()=>{
+    // advGetAdType().then(res=>{
+    //   if(res.code==0){
+    //     let obj={}
+    //     res.data?.map((item: { code: string | number; title: string })=>{
+    //       obj[item.code]=item.title
+    //     })
+    //     setAdvSelect(obj)
+    //   }
+    // })
+  },[])
+
+  const tableColumns: ProColumns[] = [
+    {
+      title: '序号',
+      dataIndex:'id',
+      hideInSearch: true
+    },
+    {
+      title: '操作类型',
+      dataIndex: 'type',
+      align: 'center',
+      hideInSearch: true
+    },
+    {
+      title: '配置类型',
+      dataIndex: 'editType',
+      align: 'center',
+      hideInSearch: true
+    },
+    {
+      title: '查看分成配置快照',
+      dataIndex: '',
+      align: 'center',
+      render: (_) => {
+        return <a onClick={()=>{  }}>查看</a>
+      },
+      hideInSearch: true,
+    },
+    {
+      title: '操作人',
+      dataIndex: 'lastEditor',
+      align: 'center',
+    },
+    {
+      title: '操作时间',
+      dataIndex: 'dataRange',
+      renderFormItem: () => <TimeSelect />,
+      align: 'center',
+      hideInTable: true,
+    },
+    {
+      title: '操作时间',
+      dataIndex: 'createTime',
+      valueType: 'text',
+      hideInSearch: true
+    }
+  ]
+
+  return (
+    <DrawerForm
+      layout="horizontal"
+      title='更新历史'
+      onVisibleChange={setVisible}
+      visible={visible}
+      width={1400}
+      drawerProps={{
+        forceRender: true,
+        destroyOnClose: true,
+      }}
+      submitter={{
+        render:()=>{
+          return []
+        }
+      }}
+    >
+      <ProTable
+        rowKey="id"
+        columns={tableColumns}
+        request={getLogListByParams}
+        columnEmptyText={false}
+        actionRef={form}
+        options={false}
+        search={{
+          defaultCollapsed: true,
+          labelWidth: 110,
+          optionRender: (searchConfig: any, formProps: any, dom: any[]) => [
+            ...dom.reverse()
+          ],
+        }}
+      />
+      {/* {detailVisible&&
+      <AdvertisementDetails
+         visible={detailVisible}
+         setVisible={setDetailVisible}
+         msgDetail={parameter}
+      />} */}
+    </DrawerForm>
+  )
+}
---
+++ src/services/product-management/transaction-sharing-management.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request';
+import moment from 'moment';
+
+export const getListByParams = async (params = {}, options = {}) => {
+  const { current, pageSize, ...rest } = params;
+  const res = await request('/auth/finance/billConfig/getListByParams', {
+    method: 'POST',
+    data: {
+        page: current,
+        size: pageSize,
+        ...rest
+    },
+    ...options
+  });
+
+  return {
+    data: res.data.records,
+    total: res.data.total
+  }
+}
+
+export const getLogListByParams = async (params = {}, options = {}) => {
+    const { current, pageSize, dataRange= [], ...rest } = params;
+    const res = await request('/auth/finance/billConfig/getLogListByParams', {
+      method: 'POST',
+      data: {
+        page: current,
+        size: pageSize,
+        ...rest,
+        startTime: dataRange[0]&& moment(dataRange[0]).valueOf(),
+        endTime: dataRange[1]&& moment(dataRange[1]).valueOf(),
+      },
+      ...options
+    });
+  
+    return {
+      data: res.data.records,
+      total: res.data.total
+    }
+  }
+
+  export const updateConfigStatusById = async (params = {}, options = {}) => {
+    const res = await request('/auth/finance/billConfig/updateConfigStatusById', {
+      method: 'POST',
+      data:params,
+      ...options
+    });
+  
+    return {
+      data: res.data,
+      code: res.code
+    }
+  }
