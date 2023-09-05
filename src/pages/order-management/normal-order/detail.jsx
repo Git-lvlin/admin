c24d79be3433c0bac43bfb61088fb351bec4a51e
GIT-LVGUOLIN <lvguolin@yongdaxing.cn>
2023-09-05 14:00:43 +0800

    fix

--- src/pages/order-management/normal-order/detail.jsx
+++ src/pages/order-management/normal-order/detail.jsx
@@ -303,8 +303,8 @@ const Detail = (props) => {
                   </div>
                 }
                 {
-                  detailData?.subType == 2001 && <div className={styles.box}>
-                    <div>《大健康区县服务协议》</div>
+                  detailData?.contractId && <div className={styles.box}>
+                    <div>法大大电子合同</div>
                     <div className={detailData?.contractUrl ?'':styles.make}>
                       {detailData?.contractUrl ? <a href={detailData?.contractUrl} target="_blank">{detailData?.contractId}</a> : '未签写'}
                     </div>
