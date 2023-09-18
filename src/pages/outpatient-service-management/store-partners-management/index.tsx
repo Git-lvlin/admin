e78271521007a358f9425276fb7fb0ab2b8f8ae4
coderby <wubinyan@qhhznt.com>
2023-09-18 16:08:12 +0800

    fixBug

--- src/pages/outpatient-service-management/store-partners-management/index.tsx
+++ src/pages/outpatient-service-management/store-partners-management/index.tsx
@@ -25,11 +25,11 @@ const StorePartnersManagement: React.FC = () => {
     const { area, signTime, ...rest } = form.current?.getFieldsValue()
     return {
       ...rest,
-      provinceId: area && area?.[0].value,
-      cityId: area && area?.[1].value,
-      areaId: area && area?.[2].value,
+      provinceId: area && area?.[0]?.value,
+      cityId: area && area?.[1]?.value,
+      areaId: area && area?.[2]?.value,
       signTimeStart: signTime && moment(signTime?.[0]).format('YYYY-MM-DD HH:mm:ss'),
-      signTimeEnd: signTime && moment(signTime?.[0]).format('YYYY-MM-DD HH:mm:ss')
+      signTimeEnd: signTime && moment(signTime?.[1]).format('YYYY-MM-DD HH:mm:ss')
     }
   }
 
